f1630d38eaadb10eea12c729eef8856607679f52
krishn <krishnasharma38801@gmail.com>
2025-09-02 09:39:52 +0530

    Update LeftAlignEqu_Triangle.py

--- LeftAlignEqu_Triangle.py
+++ LeftAlignEqu_Triangle.py
@@ -3,9 +3,7 @@ for i in range(1,n+1):
     for j in range(i):
         print("*",end=" ")
     print()
-    
-n = 4
-for i in range(1,n+1):
-    for j in range(n+1-i):
+for i in range(n):
+    for j in range(n-1-i):
         print("*",end=" ")
     print()       
